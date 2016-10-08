a2de9a2957b4c2f11ce78d0cd2540c51986e160e
jsntxzx <jsntxzx@sina.com>
2016-10-08 15:29:19 +0000

    add nohup

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 mysql --user=root --password=root -e "source db.sql"
 
 mkdir -pv run/biquge
-scrapy crawl biqugecrawler -s JOBDIR=run/biquge
+nohup scrapy crawl biqugecrawler -s JOBDIR=run/biqugei >/dev/null 2>&1 &
